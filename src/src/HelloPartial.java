130bd7d0949a72e5e834f73e50f4eab99f7eb44a
Tobi's tool <Tobi@example.com>
2020-05-27 23:25:09 +0200

    Automatically created commit.

--- src/src/HelloPartial.java
+++ src/src/HelloPartial.java
@@ -8,7 +8,7 @@ public class HelloPartial {
     //&begin(FeatureA)
     private void methodA(){
         System.out.println("methodA");
-		System.out.println("methodA2");
+		System.out.println("New Method here");
     }
     //&end(FeatureA)
     
