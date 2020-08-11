ec57758caf38e92b98a3f128632919af66506931
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-08-11 19:42:35 +0200

    More test data

---
+++ src/src/ConfigurationTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigurationTest {
+
+    @org.junit.jupiter.api.Test
+    void getOwnName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSendCoinsAutoclose() {
+    }
+}
